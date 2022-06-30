24d408e29673639640d6d72722b4f1f912129a17
Everson Silva <everson_silva2009@hotmail.com>
2022-06-30 16:28:57 -0300

    Refatorando a funcionalidade de criação de livros.

---
+++ src/main/java/com/eversonsilva/bookstoremanager/controller/BookController.java
@@ -0,0 +1,29 @@
+package com.eversonsilva.bookstoremanager.controller;
+
+import com.eversonsilva.bookstoremanager.dto.MessageResponseDTO;
+import com.eversonsilva.bookstoremanager.entity.Book;
+import com.eversonsilva.bookstoremanager.repository.BookRepository;
+import com.eversonsilva.bookstoremanager.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public MessageResponseDTO create(@RequestBody Book book){
+        return bookService.create(book);
+    }
+
+}
